1e42d7701460c677a100dbb0467ca838802a649b
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-08-07 00:22:35 +0100

    Updates

---
+++ precache-manifest.0e75bac4713557247f64978d4edc643f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e40cc5c87d86fc34366f44d3ece9b9",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "3527c8753fabec582c2b",
+    "url": "/react-demo/static/css/main.4918906d.chunk.css"
+  },
+  {
+    "revision": "10bc09a26f6afac44073",
+    "url": "/react-demo/static/js/2.1f609ec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.1f609ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3527c8753fabec582c2b",
+    "url": "/react-demo/static/js/main.ad1faf34.chunk.js"
+  },
+  {
+    "revision": "e91f5e23d011ab615da4",
+    "url": "/react-demo/static/js/runtime-main.5f8d9a6d.js"
+  }
+]);
