c8e400be70bce9f167d24fcc460787456528a178
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-09-08 16:40:11 +0100

    Updates

--- precache-manifest.94b172fc7fa6212ebd423813ee471985.js
+++ precache-manifest.94b172fc7fa6212ebd423813ee471985.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed90d4cd2d2ff5eec0787f2d8b30eadb",
+    "revision": "e6c8d3122ceb700a7a7456b72d6893d2",
     "url": "/react-demo/index.html"
   },
   {
-    "revision": "f15ea520aa1f33eb6d4b",
-    "url": "/react-demo/static/css/main.0da9da29.chunk.css"
+    "revision": "dbec844a65c1f8e5b2ca",
+    "url": "/react-demo/static/css/main.b641bf43.chunk.css"
   },
   {
     "revision": "16aeebc164160163ad2d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-demo/static/js/2.741da1cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f15ea520aa1f33eb6d4b",
-    "url": "/react-demo/static/js/main.14499c45.chunk.js"
+    "revision": "dbec844a65c1f8e5b2ca",
+    "url": "/react-demo/static/js/main.8e5fab66.chunk.js"
   },
   {
     "revision": "e91f5e23d011ab615da4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-demo/precache-manifest.b033c470474b231f0d2c23616b6f335b.js"
+  "/react-demo/precache-manifest.94b172fc7fa6212ebd423813ee471985.js"
 );
 
 self.addEventListener('message', (event) => {
